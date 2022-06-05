4c5871adef321f881443fa7ca94e33aac04322e0
eftekin <75522456+eftekin@users.noreply.github.com>
2022-06-06 00:01:34 +0300

    first commit

---
+++ DogYears.swift
@@ -0,0 +1,12 @@
+//Patşah 14 years old.
+var dogAge = 14
+
+let earlyYears = 21
+var laterYears = (dogAge - 2)*4
+
+
+let humanYears = earlyYears + laterYears
+
+print("My name is Patşah! Ruff ruff, I am \(humanYears) years old in human years.")
+
+
