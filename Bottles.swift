21fc21f879f09082d4282dbdcbabdfdefebcf6fa
eftekin <75522456+eftekin@users.noreply.github.com>
2022-06-10 22:47:05 +0300

    99 bottles of beer song

---
+++ Bottles.swift
@@ -0,0 +1,12 @@
+var numBottles = 99
+
+while numBottles > 0 {
+
+  print ("\(numBottles) bottles of beer on the wall, \(numBottles) bottles of beer!")
+
+  print ("You take one down and pass it around...")
+
+  numBottles -= 1
+  
+  print("\(numBottles) bottles of beer on the wall!\n")
+}
