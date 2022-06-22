dd4090bd1745ec24813e2d21831d255b924d7b0e
eftekin <75522456+eftekin@users.noreply.github.com>
2022-06-22 19:05:46 +0300

    Palindrome identifier

---
+++ Palindrome.swift
@@ -0,0 +1,15 @@
+var text = ["r", "a", "c", "e", "c", "a", "r"]
+var reversed = [String]()
+var counter = text.count - 1
+
+while counter >= 0 {
+  reversed.append(text[counter])
+  counter -= 1
+
+}
+if text == reversed {
+  print("We have a palindrome!")
+}
+else {
+  print("\(text) is not palindrome.")
+}
